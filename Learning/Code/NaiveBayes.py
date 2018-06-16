81143e47854f07433459ad6a8a6b5dc6a370c95f
Jason Shipp <shippj@purdue.edu>
2018-06-16 08:05:06 -0400

    Added Naive Bayes Code for Scikit and started adding tensorflow

--- Learning/Code/NaiveBayes.py
+++ Learning/Code/NaiveBayes.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import metrics, preprocessing
 
+#SCIKIT-LEARN PORTION
+#THE FOLLOWING CODE IS ADAPTED FROM RITCHIE NG AND DOES NOT BELONG TO THE REPO OWNER 
+#Resource: https://www.ritchieng.com/machine-learning-multinomial-naive-bayes-vectorization/
 #Read in the data into a pandas dataframe
 dataFile = 'https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv'
 features = ['label', 'message']
-texts = pd.read_table(dataFile, header=None, names=features)
+sms = pd.read_table(dataFile, header=None, names=features)
 
 #If you want to see it's size
 #texts.shape
@@ -19,12 +26,82 @@ texts = pd.read_table(dataFile, header=None, names=features)
 #Lets convert the discrete "lable" into a numerical value we can work with
 #Some examples will show you doing this as a .map and then doing it manually but this 
 #Should work in the general sense
-sms['labelNum'] = sms.label.astype('category')
+le = preprocessing.LabelEncoder()
+sms['labelNum'] = le.fit_transform(sms['label'])
 
 
-#Let's use a classic example with the iris set
-iris = load_iris()
-X = iris.data
-y = iris.target
+X = sms.message
+y = sms.labelNum
+
+#Split into training and test states, you can set a random_state to have repeatable
+#occurrences
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+print(y_test.head())
+#Here we are going to use a "Count Vectorizer" to create a bag of words model
+#since we are dealing with textual data
+#This will be a "sparse matrix" and its best to let sklearn/pandas handle this as
+#it knows how to store it in an effecient manner
+vect = CountVectorizer()
+X_train_dtm = vect.fit_transform(X_train)
+X_test_dtm = vect.transform(X_test)
+
+#Let's create a model
+nb = MultinomialNB()
+nb.fit(X_train_dtm, y_train)
+
+#Here is where we make an actual prediction on our test data, easy right
+y_pred = nb.predict(X_test_dtm)
+
+#How'd we do? 
+print('Multivariate Naive Bayes')
+print('Accuracy: ',metrics.accuracy_score(y_test, y_pred))
+print('Precision: ', metrics.precision_score(y_test, y_pred))
+print('F1 Score: ', metrics.f1_score(y_test, y_pred))
+
+#Top left is True Negative predictions, top right is False Positives
+#Bottom left is False Negatives, bottom right is True Positives
+print(metrics.confusion_matrix(y_test, y_pred))
+
+#If you want to test the probability a sentence will have a certain category you can
+#Use this, however this really isn't a very good use of naive bayes
+#y_pred_prob = nb.predict_proba(X_test_dtm)[:, 1]
+
+#You can also use Gaussian NB if our matrix wasn't sparse.
+'''
+gnb = GaussianNB()
+gnb.fit(X_train_dtm, y_train)
+y_pred = nb.predict(X_test_dtm)
+print('Gaussian Naive Bayes')
+print('Accuracy: ',metrics.accuracy_score(y_test, y_pred))
+print('Precision: ', metrics.precision_score(y_test, y_pred))
+print('F1 Score: ', metrics.f1_score(y_test, y_pred))
+'''
+
+#TENSORFLOW Portion
+#This is usually not the use for Tensorflow, but you can kind of force it if you want
+class NaiveBayesClassifier:
+    dist = None
+
+    def fit(self, X, y):
+        ydist = np.unique(y)
+        count = np.array([
+            [x for x,t in zip(X,y) if t == c]
+            for c in ydist])
+
+        mean, var = tf.nn.moments(tf.constant(count), axes=[1])
+        self.dist = tf.distributions.Normal(loc=mean, scale=tf.sqrt(var))
+
+    def predict(self, X):
+        assert self.dist is not None
+        nb_classes, nb_features = map(int, self.dist.scale.shape)
+        prob = tf.reduce_sum(
+                self.dist.log_prob(
+                    tf.reshape(
+                        tf.tile(X, [1, nb_classes]), [-1, nb_classes, nb_features])),
+                    axis=2)
+        priors = np.log(np.array([1.0/nb_classes] * nb_classes))
+        joint_likelihood = tf.add(priors, cond_probs)
+#TODO
+        norm_factor = tf.reduce logsumexp
 
 
--- Learning/Vocab.md
+++ Learning/Vocab.md
@@ -2,8 +2,46 @@
 This will be in my own words, and updated upon request if something I write is
 insufficent or incorrect. I'm not doing any lookups on this stuff so it's
 possible!
-  * Feature:
+* Feature:
     A Feature is literally any piece of relevant information that we have on a
     data point. This can be discrete (an integer or floating point value) or
     categorical (Red vs. Blue / True vs. False). We usually choose to encode all
     categorical values if we care about them. 
+* Sparse Matrix: 
+    This term most often comes up when you are discussing textual data sets.
+    When looking at this type of data your best bet is to represent the data as
+    a bag of words, which means each word gets a column, and the data point in
+    that column is how often it is used in that data point (sentence, paper,
+    whatever). This means that you are going to have a lot of 0's in every
+    single row. This is a tremendous waste of space in general if you are using
+    native arrays or tables, so pandas/numpy/scipy/sklearn store it as a special data
+    type called a Sparse Matrix which is usually written in Fortran and much
+    more performant, speed and storage wise. You can read more [here](https://machinelearningmastery.com/sparse-matrices-for-machine-learning/)
+
+* Recall: 
+    //TODO
+
+* Accuracy: 
+    //TODO
+
+* Precision:
+    //TODO
+
+* ROC: 
+    More formally known as the "Receiver Operating Characteristic Curve", the
+    ROC curve is a plot whose goal is to illustrate the diagnostic ability of a
+    classifier as its threshold is varied. You create it by plotting the True
+    Positive Rate (recall) agains the False Positive Rate (fall-out, Type I
+    error)
+
+* AUC: 
+    The abbreviated version of "Area Under Curve" is a metric used for
+    classifiers when the label you are aiming for is binary (0,1). It's a good
+    metric after accuracy or f1 scores but is less natural. It's measured in the
+    real range [0,1] just like the above mentioned, however its calculation is
+    much different. In this case the area under the curve refers the the area
+    underneath a ROC curve (explained above). AUC is especially useful if your
+    classifier is particularly label heavy towards one of the two choices. A
+    mediocre ML algorithm can score high accuracy by learning to just choose
+    true a bunch of times, but if that is the case it will score a 0.5 in AUC.
+    [Resource](http://fastml.com/what-you-wanted-to-know-about-auc/) 
