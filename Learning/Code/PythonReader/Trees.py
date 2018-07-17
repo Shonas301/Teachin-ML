abe4bda4a6a6bfcee192992bd62e071ae06743e1
Jason Shipp <shippj@purdue.edu>
2018-07-17 03:15:32 -0400

    added bst delete

--- Learning/Code/PythonReader/Trees.py
+++ Learning/Code/PythonReader/Trees.py
@@ -59,7 +59,7 @@ class BST:
             self.left.insert(data)
         elif data <= self.data:
             self.left = BST(data)
-        elif value > self.data and self.right:
+        elif data > self.data and self.right:
             self.right.insert(data)
         else:
             self.right = BST(data)
@@ -71,3 +71,42 @@ class BST:
             return self.right.find(data)
 
         return data == self.data
+
+    def clear(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+    def delete(self, data, parent):
+        if data < self.data and self.left:
+            return self.left.remove_node(data, self)
+        elif data < self.data:
+            return False
+        elif data > self.data and self.right:
+            return self.right.remove_node(data, self)
+        elif data > self.data:
+            return False
+        else:
+            if self.left is None and self.right is None and self == parent.left:
+                parent.left = None
+                self.clear()
+            elif self.left is None and self.right is None and self == parent.right:
+                parent.right = None
+                self.clear()
+            elif self.left and self.right is None and self == parent.left:
+                parent.left = self.left
+                self.clear()
+            elif self.left and self.right is None and self == parent.right:
+                parent.right = self.left
+                self.clear()
+            elif self.right and self.left is None and self == parent.left:
+                parent.left = self.right
+                self.clear()
+            elif self.right and self.left is None and self == parent.right:
+                parent.right = self.right
+                self.clear()
+            else:
+                self.data = self.right.find_minimum_data()
+                self.right.remove_node(self.data, self)
+
+        return True
